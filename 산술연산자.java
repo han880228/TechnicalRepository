9e2566943f76a2f9713388f6bb81e7bfcfda270a
han880228 <84111458+han880228@users.noreply.github.com>
2021-05-13 23:51:05 +0900

    [산술예제] Baseline코드 (v1.0)
    데이터타입에 따른 산술연산코드

---
+++ 산술연산자.java
@@ -0,0 +1,33 @@
+
+public class 산술연산자 {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 6;
+		int result1 = a + b;
+		
+		System.out.println("result = " + result1);
+		
+		float c = 1.7f;
+		float d = 2.3f;
+		float result2 = c * d;
+		
+		System.out.println("result = " + result2);
+		
+		double e = 31.0;
+		double f = 4.0;
+		double result3 = e / f;
+		
+		System.out.println("result = " + result3);
+		
+		int g = 26;
+		int h = 3;
+		int result4 = g % h;
+		
+		System.out.println("result = " + result4);
+		
+
+	}
+
+}
